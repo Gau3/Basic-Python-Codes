e7e65541b10a88b8a7a3c876127808bf9584e2db
TheBeginner98 <87329744+TheBeginner98@users.noreply.github.com>
2021-07-28 21:56:27 +0530

    Delete Binary todecimal.py

--- Binary todecimal.py
+++
@@ -1,9 +0,0 @@
-def binaryToDecimal(binary:int)->int: 
-def binaryToDecimal(binary): 
-    i += 1
-	print(decimal)	 
-
-if __name__ == '__main__': 
-	import doctest
-	doctest.testmod()  
-binaryToDecimal(100)
