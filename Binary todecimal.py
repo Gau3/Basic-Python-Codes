af9995c3c29a93494659fb63de7d61d325814460
TheBeginner98 <87329744+TheBeginner98@users.noreply.github.com>
2021-07-28 21:52:39 +0530

    Add files via upload

---
+++ Binary todecimal.py
@@ -0,0 +1,9 @@
+def binaryToDecimal(binary:int)->int: 
+def binaryToDecimal(binary): 
+    i += 1
+	print(decimal)	 
+
+if __name__ == '__main__': 
+	import doctest
+	doctest.testmod()  
+binaryToDecimal(100)
